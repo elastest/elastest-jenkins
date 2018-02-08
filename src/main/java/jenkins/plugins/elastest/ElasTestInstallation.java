0eeb0deb65de671386ce5694bde166bf1fd81827
franciscoRdiaz <francisco.diazbayon@gmail.com>
2018-02-08 14:30:57 +0100

    refactor: change license data

--- src/main/java/jenkins/plugins/elastest/ElasTestInstallation.java
+++ src/main/java/jenkins/plugins/elastest/ElasTestInstallation.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2017 Hewlett-Packard Development Company, L.P.
+ * (C) Copyright 2017-2019 ElasTest (http://elastest.io/)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
